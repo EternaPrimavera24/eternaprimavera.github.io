0d04e46cedd416523709a33c1558a537fe14b891
EternaPrimavera24 <eternaprimaverasalondeeventos@gmail.com>
2024-10-29 19:56:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+const fulImgBox = document.getElementById("fulImgBox"),
+    fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference) {
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg() {
+    fulImgBox.style.display = "none";
+}
